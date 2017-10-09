ffbca8efd3ed6e0cc5309db4decef0bd6a2c39fa
STENUNGSUND\johan.hagerborg <johanhagerborg@gmail.com>
2017-10-09 10:38:33 +0200

    Kopplat inmatade värden till login

---
+++ Photo_Database/Static/JavaScript/Main.js
@@ -0,0 +1,7 @@
+﻿$("#login").click(function () {
+
+    let name = $("#name").val()
+    let context = $("#context").val()
+
+    console.log("Loggar in. Namnet är " + name + " och kontext är " + context);
+})
